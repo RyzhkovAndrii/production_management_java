0a45e2043b345b8bcced6abef512ea3c0c76e75e
Olesiap <pryadilnikova@gmail.com>
2018-04-02 00:19:53 +0300

    Added BaseEntity, postgreSQL`s properties to application.properties

--- src/main/java/ua/com/novopacksv/production/model/Client.java
+++ src/main/java/ua/com/novopacksv/production/model/Client.java
@@ -1,7 +1,22 @@
 package ua.com.novopacksv.production.model;
 
-public class Client {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
-    private Long id;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@EqualsAndHashCode(callSuper = true)
+@Table(name = "client")
+public class Client extends BaseEntity{
+
+    @Column(name = "name")
     private String name;
 }
--- src/main/java/ua/com/novopacksv/production/model/Modification.java
+++ src/main/java/ua/com/novopacksv/production/model/Modification.java
@@ -1,11 +1,25 @@
 package ua.com.novopacksv.production.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 import java.util.Date;
 
-public class Modification {
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@EqualsAndHashCode(callSuper = true)
+@javax.persistence.Table(name = "modification")
+public class Modification extends BaseEntity{
 
-    private Long id;
+    @Column(name = "user")
     private User user;
+    @Column(name = "modification_date")
     private Date modificationDate;
+    @Column(name = "table_date")
     private Table tableType;
 }
