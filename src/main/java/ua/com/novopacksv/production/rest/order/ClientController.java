7c9250018a25aceee46870a729c988ecc8b1ddd7
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-04-02 19:06:29 +0300

    client controller methods were created

--- src/main/java/ua/com/novopacksv/production/rest/order/ClientController.java
+++ src/main/java/ua/com/novopacksv/production/rest/order/ClientController.java
@@ -1,11 +1,16 @@
 package ua.com.novopacksv.production.rest.order;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import ua.com.novopacksv.production.service.order.ClientService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import ua.com.novopacksv.production.converter.ModelConversionService;
+import ua.com.novopacksv.production.dto.order.ClientRequest;
+import ua.com.novopacksv.production.dto.order.ClientResponse;
+import ua.com.novopacksv.production.service.order.ClientService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/clients", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
@@ -15,5 +20,41 @@ public class ClientController {
     private final ClientService clientService;
 
     private final ModelConversionService conversionService;
+
+    @GetMapping
+    public ResponseEntity<List<ClientResponse>> getList() {
+        List<Client> clients = clientService.findAll();
+        List<ClientResponse> response = conversionService.convert(clients, ClientResponse.class);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClientResponse> getById(@PathVariable Long id) {
+        Client client = clientService.getById(id);
+        ClientResponse response = conversionService.convert(client, ClientResponse.class);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientResponse> save(@RequestBody ClientRequest request) {
+        Client client = conversionService.convert(request, Client.class);
+        client = clientService.save(client);
+        ClientResponse response = conversionService.convert(client, ClientResponse.class);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity update(@PathVariable Long id, @RequestBody ClientRequest request) {
+        Client client = conversionService.convert(request, Client.class);
+        client.setId(id);
+        clientService.update(client);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/id")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        clientService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
 }
