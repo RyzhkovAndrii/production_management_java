aeef8f469cde8f1d181f5f507d525a004c1fc2bf
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-04-02 23:29:47 +0300

    models were imported

--- src/main/java/ua/com/novopacksv/production/service/order/OrderItemService.java
+++ src/main/java/ua/com/novopacksv/production/service/order/OrderItemService.java
@@ -1,4 +1,7 @@
 package ua.com.novopacksv.production.service.order;
 
+import ua.com.novopacksv.production.model.orderModel.OrderItem;
+import ua.com.novopacksv.production.service.BaseEntityService;
+
 public interface OrderItemService extends BaseEntityService<OrderItem> {
 }
--- src/main/java/ua/com/novopacksv/production/service/order/OrderService.java
+++ src/main/java/ua/com/novopacksv/production/service/order/OrderService.java
@@ -1,5 +1,8 @@
 package ua.com.novopacksv.production.service.order;
 
+import ua.com.novopacksv.production.model.orderModel.Order;
+import ua.com.novopacksv.production.service.BaseEntityService;
+
 public interface OrderService extends BaseEntityService<Order> {
 
     void addOrderItemToOrder(Long orderId, Long OrderItemId);
