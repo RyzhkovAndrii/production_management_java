fc4e764750f343aa52c556dfe0aff0db286f03cc
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-04-02 19:28:40 +0300

    methods of OrderItemController were created

--- src/main/java/ua/com/novopacksv/production/rest/order/OrderItemController.java
+++ src/main/java/ua/com/novopacksv/production/rest/order/OrderItemController.java
@@ -1,11 +1,15 @@
 package ua.com.novopacksv.production.rest.order;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import ua.com.novopacksv.production.service.order.OrderItemService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import ua.com.novopacksv.production.converter.ModelConversionService;
+import ua.com.novopacksv.production.dto.order.OrderItemRequest;
+import ua.com.novopacksv.production.dto.order.OrderItemResponse;
+import ua.com.novopacksv.production.model.OrderItem;
+import ua.com.novopacksv.production.service.order.OrderItemService;
 
 @RestController
 @RequestMapping(value = "/order-items", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
@@ -15,5 +19,28 @@ public class OrderItemController {
     private final OrderItemService orderItemService;
 
     private final ModelConversionService conversionService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderItemResponse> getById(@PathVariable Long id) {
+        OrderItem orderItem = orderItemService.getById(id);
+        OrderItemResponse response = conversionService.convert(orderItem, OrderItemResponse.class);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderItemResponse> save(@RequestBody OrderItemRequest request) {
+        OrderItem orderItem = conversionService.convert(request, OrderItem.class);
+        orderItem = orderItemService.save(orderItem);
+        OrderItemResponse response = conversionService.convert(orderItem, OrderItemResponse.class);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity update(@PathVariable Long id, @RequestBody OrderItemRequest request) {
+        OrderItem orderItem = conversionService.convert(request, OrderItem.class);
+        orderItem.setId(id);
+        orderItemService.update(orderItem);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
 }
