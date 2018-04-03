16f9a000ac1e72e3981afceebf8372bb387eec4e
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-04-03 19:53:52 +0300

    date and time converters to string were added

---
+++ src/main/java/ua/com/novopacksv/production/converter/datetime/LocalDateTimeToStringConverter.java
@@ -0,0 +1,21 @@
+package ua.com.novopacksv.production.converter.datetime;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class LocalDateTimeToStringConverter implements Converter<LocalDateTime, String> {
+
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    @Nullable
+    @Override
+    public String convert(@Nullable LocalDateTime localDateTime) {
+        return localDateTime == null ? null : localDateTime.format(formatter);
+    }
+
+}
---
+++ src/main/java/ua/com/novopacksv/production/converter/datetime/StringToLocalDateTimeConverter.java
@@ -0,0 +1,21 @@
+package ua.com.novopacksv.production.converter.datetime;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class StringToLocalDateTimeConverter implements Converter<String, LocalDateTime> {
+
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    @Nullable
+    @Override
+    public LocalDateTime convert(@Nullable String source) {
+        return (source == null || source.isEmpty()) ? null : LocalDateTime.parse(source, formatter);
+    }
+
+}
