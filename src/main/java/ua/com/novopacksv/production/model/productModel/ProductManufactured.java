66072f313baa4d8529d306ad481d14a6ba7dd9a6
Olesiap <pryadilnikova@gmail.com>
2018-04-04 00:07:33 +0300

    model 1+

--- src/main/java/ua/com/novopacksv/production/model/productModel/ProductManufactured.java
+++ src/main/java/ua/com/novopacksv/production/model/productModel/ProductManufactured.java
@@ -2,5 +2,8 @@ package ua.com.novopacksv.production.model.productModel;
 
 import ua.com.novopacksv.production.model.BaseEntity;
 
+import javax.persistence.Table;
+
+@Table(name = "product_manufactured")
 public class ProductManufactured extends BaseEntity{
 }
