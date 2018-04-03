bfaeddd3bf5b67876f6ad91acef515b1d77ce851
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-04-03 19:30:07 +0300

    extended from BaseEntityResponse was added

--- src/main/java/ua/com/novopacksv/production/dto/roll/RollTypeResponse.java
+++ src/main/java/ua/com/novopacksv/production/dto/roll/RollTypeResponse.java
@@ -2,10 +2,11 @@ package ua.com.novopacksv.production.dto.roll;
 
 import lombok.Getter;
 import lombok.Setter;
+import ua.com.novopacksv.production.dto.BaseEntityResponse;
 
 @Getter
 @Setter
-public class RollTypeResponse {
+public class RollTypeResponse extends BaseEntityResponse {
 
     private Double thickness;
 
