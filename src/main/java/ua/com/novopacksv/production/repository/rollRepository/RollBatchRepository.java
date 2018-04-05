9bfc1a85e08e93b1e7c4bd99f810533141c54fa9
Olesiap <pryadilnikova@gmail.com>
2018-04-05 23:16:14 +0300

    made repositories to folder rollRepository

--- src/main/java/ua/com/novopacksv/production/repository/rollRepository/RollBatchRepository.java
+++ src/main/java/ua/com/novopacksv/production/repository/rollRepository/RollBatchRepository.java
@@ -3,12 +3,14 @@ package ua.com.novopacksv.production.repository.rollRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ua.com.novopacksv.production.model.rollModel.RollBatch;
+import ua.com.novopacksv.production.model.rollModel.RollType;
 
-import java.time.LocalDate;
 import java.util.List;
 
-@Repository
-public interface RollBatchRepository extends JpaRepository <RollBatch, Long> {
 
-    List<RollBatch> findAllByManufacturedDateBetween(LocalDate fromDate, LocalDate toDate);
+@Repository
+public interface RollBatchRepository extends JpaRepository<RollBatch, Long> {
+
+    List<RollBatch> findAllByRollManufactured_RollType(RollType rollType);
+
 }
---
+++ src/main/java/ua/com/novopacksv/production/repository/rollRepository/RollOperationRepository.java
@@ -0,0 +1,20 @@
+package ua.com.novopacksv.production.repository.rollRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ua.com.novopacksv.production.model.rollModel.OperationType;
+import ua.com.novopacksv.production.model.rollModel.RollOperation;
+import ua.com.novopacksv.production.model.rollModel.RollType;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface RollOperationRepository extends JpaRepository<RollOperation, Long> {
+
+    List<RollOperation> findAllByOperationTypeAndOperationDateBetween
+            (OperationType operationType, LocalDate fromDate, LocalDate toDate);
+
+    List<RollOperation> findAllByRollManufactured_RollTypeAndOperationDateBetween
+            (RollType rollType, LocalDate fromDate, LocalDate toDate);
+}
