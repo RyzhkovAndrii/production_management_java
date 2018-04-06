1326c68c042a274b4873341726342f2aa93c0b5e
Olesiap <pryadilnikova@gmail.com>
2018-04-06 21:08:58 +0300

    deleted annotations @Entity, @Table, added method for count leftover in RollBatch

--- src/main/java/ua/com/novopacksv/production/model/rollModel/RollBatch.java
+++ src/main/java/ua/com/novopacksv/production/model/rollModel/RollBatch.java
@@ -6,27 +6,24 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import ua.com.novopacksv.production.model.BaseEntity;
 
-import javax.persistence.*;
-import javax.persistence.Table;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
 @EqualsAndHashCode(callSuper = true)
-@Table(name = "roll_batch")
 public class RollBatch extends BaseEntity {
 
-    @OneToOne
-    @PrimaryKeyJoinColumn
     private RollManufactured rollManufactured;
 
-    @Column(name = "manufactured_amount")
     private Integer manufacturedAmount;
 
-    @Column(name = "used_amount")
     private Integer usedAmount;
 
-    @Column(name = "left_over_amount")
-    private Integer leftOverAmount;
+    private Integer leftOverAmount = countLeftOverAmount(manufacturedAmount, usedAmount);
+
+    private Integer countLeftOverAmount(Integer manufacturedAmount, Integer usedAmount){
+        if(usedAmount<= manufacturedAmount){
+            return manufacturedAmount-usedAmount;
+        }
+        return 0;
+    }
 }
