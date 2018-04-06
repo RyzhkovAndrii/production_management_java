782286e46c722f3ce1b4d2541da1b837ef943bd7
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-04-06 16:19:26 +0300

    added converter for roll_left_over response

---
+++ src/main/java/ua/com/novopacksv/production/converter/roll/RollLeftOverToRollLeftOverResponseConverter.java
@@ -0,0 +1,26 @@
+package ua.com.novopacksv.production.converter.roll;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import ua.com.novopacksv.production.dto.roll.RollLeftOverResponse;
+import ua.com.novopacksv.production.model.rollModel.RollLeftOver;
+
+@Component
+public class RollLeftOverToRollLeftOverResponseConverter implements Converter<RollLeftOver, RollLeftOverResponse> {
+
+    @Autowired
+    private ConversionService conversionService;
+
+    @Override
+    public RollLeftOverResponse convert(RollLeftOver source) {
+        String date = conversionService.convert(source.getDate(), String.class);
+        RollLeftOverResponse result = new RollLeftOverResponse();
+        result.setDate(date);
+        result.setRollTypeId(source.getRollType().getId());
+        result.setAmount(source.getAmount());
+        return result;
+    }
+
+}
