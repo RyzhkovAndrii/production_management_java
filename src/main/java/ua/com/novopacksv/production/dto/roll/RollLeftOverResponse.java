a6b015b7dfe2aec452f8f0e403b96c49de14f18c
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-04-06 16:14:44 +0300

    added field "date"

--- src/main/java/ua/com/novopacksv/production/dto/roll/RollLeftOverResponse.java
+++ src/main/java/ua/com/novopacksv/production/dto/roll/RollLeftOverResponse.java
@@ -8,6 +8,8 @@ import ua.com.novopacksv.production.dto.BaseEntityResponse;
 @Setter
 public class RollLeftOverResponse extends BaseEntityResponse {
 
+    private String date;
+
     private Long rollTypeId;
 
     private Integer amount;
