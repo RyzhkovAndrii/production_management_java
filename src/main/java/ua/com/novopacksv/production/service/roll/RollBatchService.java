7aadff05a197b3a37cf41483dde401eb38e51258
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-04-10 22:13:13 +0300

    added some methods needed in controllers

--- src/main/java/ua/com/novopacksv/production/service/roll/RollBatchService.java
+++ src/main/java/ua/com/novopacksv/production/service/roll/RollBatchService.java
@@ -3,5 +3,17 @@ package ua.com.novopacksv.production.service.roll;
 import ua.com.novopacksv.production.model.rollModel.RollBatch;
 import ua.com.novopacksv.production.service.BaseEntityService;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface RollBatchService extends BaseEntityService<RollBatch> {
+
+    List<RollBatch> findAllByManufacturedDate(LocalDate date);
+
+    List<RollBatch> findAllByRollTypeIdAndManufacturedPeriod(Long rollTypeId, LocalDate fromDate, LocalDate toDate);
+
+    List<RollBatch> findAllByManufacturedPeriod(LocalDate fromDate, LocalDate toDate);
+
+    RollBatch findByRollTypeIdAndManufacturedDate(Long rollTypeId, LocalDate date);
+
 }
--- src/main/java/ua/com/novopacksv/production/service/roll/RollLeftOverService.java
+++ src/main/java/ua/com/novopacksv/production/service/roll/RollLeftOverService.java
@@ -3,5 +3,13 @@ package ua.com.novopacksv.production.service.roll;
 import ua.com.novopacksv.production.model.rollModel.RollLeftOver;
 import ua.com.novopacksv.production.service.BaseEntityService;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface RollLeftOverService extends BaseEntityService<RollLeftOver> {
+
+    List<RollLeftOver> findAllByDate(LocalDate date);
+
+    RollLeftOver findByRollTypeIdAndDate(Long rollTypeId, LocalDate date);
+
 }
