3491bc93c433dd7a231bc5065c8e553aa929501f
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-04-11 11:53:52 +0300

    added realization for methods

--- src/main/java/ua/com/novopacksv/production/service/roll/RollTypeServiceImpl.java
+++ src/main/java/ua/com/novopacksv/production/service/roll/RollTypeServiceImpl.java
@@ -1,38 +1,48 @@
 package ua.com.novopacksv.production.service.roll;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ua.com.novopacksv.production.exception.ResourceNotFoundException;
 import ua.com.novopacksv.production.model.rollModel.RollType;
+import ua.com.novopacksv.production.repository.rollRepository.RollTypeRepository;
 
 import java.util.List;
 
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class RollTypeServiceImpl implements RollTypeService {
+
+    private final RollTypeRepository rollTypeRepository;
 
     @Override
     public RollType findById(Long id) {
-        return null;
+        return rollTypeRepository.findById(id).orElseThrow(() -> {
+            String message = String.format("Roll Type whit id = %d not found!", id);
+            return new ResourceNotFoundException(message);
+        });
     }
 
     @Override
     public List<RollType> findAll() {
-        return null;
+        return rollTypeRepository.findAll();
     }
 
     @Override
     public RollType save(RollType rollType) {
-        return null;
+        return rollTypeRepository.save(rollType);
     }
 
     @Override
     public RollType update(RollType rollType) {
-        return null;
+        return this.save(rollType);
     }
 
     @Override
     public void delete(Long id) {
-
+        RollType rollType = findById(id);
+        rollTypeRepository.delete(rollType);
     }
 
 }
