b841b8cb42fab4ad6c78636c510a4394b333467f
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-04-12 10:58:38 +0300

    added findByManufacturedDateAndRollType method

--- src/main/java/ua/com/novopacksv/production/service/roll/RollManufacturedService.java
+++ src/main/java/ua/com/novopacksv/production/service/roll/RollManufacturedService.java
@@ -1,6 +1,7 @@
 package ua.com.novopacksv.production.service.roll;
 
 import org.springframework.stereotype.Service;
+import ua.com.novopacksv.production.exception.ResourceNotFoundException;
 import ua.com.novopacksv.production.model.rollModel.RollManufactured;
 import ua.com.novopacksv.production.model.rollModel.RollType;
 
@@ -11,6 +12,9 @@ import java.util.List;
 public interface RollManufacturedService {
 
     RollManufactured findByManufacturedDateAndRollTypeOrCreateNew(LocalDate manufacturedDate, RollType rollType);
+
+    RollManufactured findByManufacturedDateAndRollType(LocalDate manufacturedDate, RollType rollType)
+            throws ResourceNotFoundException;
 
     List<RollManufactured> findAllByManufacturedDate(LocalDate date);
 
