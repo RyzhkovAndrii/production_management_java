50754f36b05636c1cd9632e2c75b21c4acffab09
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-04-13 22:48:10 +0300

    added scheduling configuration (scheduling enabled)

---
+++ src/main/java/ua/com/novopacksv/production/config/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package ua.com.novopacksv.production.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
