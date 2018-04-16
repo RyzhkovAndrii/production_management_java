715ed8d39bc0514efa80dba83398be3541b768f2
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-04-16 21:13:46 +0300

    added russian comments

--- src/main/java/ua/com/novopacksv/production/service/roll/RollBatchServiceImpl.java
+++ src/main/java/ua/com/novopacksv/production/service/roll/RollBatchServiceImpl.java
@@ -11,25 +11,64 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Класс имплементирует методы интерфейса {@code RollBatchService}.
+ * Содержит методы возвращающие информацию о количестве произведнных, списанных в производство и оставшихся
+ * на данный момент рулонов определенного типа, даты производства, периода производства и т.п.
+ * Методы класса возвращают сущность {@see RollBatch}, содержащую вышеуказнную информацию,
+ * либо {@code List} таких сущностей
+ */
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class RollBatchServiceImpl implements RollBatchService {
 
+    /**
+     * Содержит методы для работы с сущностью {@code RollManufactured}
+     */
     private final RollManufacturedService rollManufacturedService;
 
+    /**
+     * Вычисляет и возвращает информацию о количестве произведнных, списанных в производство и оставшихся
+     * на данный момент рулонов для указанных типа рулона и даты производства
+     * 
+     * @param rollTypeId ID типа рулона
+     * @param manufacturedDate дата производства рулона
+     * @return {@code RollBatch} для указнного id типа рулона и даты производства
+     * @throws ResourceNotFoundException если рулоны с указанными id типа рулона и датой производства не найдены
+     * @throws IllegalArgumentException если rollTypeId - null и/или manufacturedDate - null
+     */
     @Override
     public RollBatch getOne(Long rollTypeId, LocalDate manufacturedDate) throws ResourceNotFoundException {
         RollManufactured rollManufactured = rollManufacturedService.findOne(manufacturedDate, rollTypeId);
         return getOne(rollManufactured);
     }
 
+    /**
+     * Вычисляет и возвращает информацию о количестве произведнных, списанных в производство и оставшихся
+     * на данный момент рулонов указанной даты производства
+     * 
+     * @param manufacturedDate дата производства рулона
+     * @return {@code List} {@code RollBatch} для указанной даты производства или empty {@code List} если в базе
+     * отсутствуют рулоны с указнной датой производства
+     * @throws IllegalArgumentException если manufacturedDate - null
+     */
     @Override
     public List<RollBatch> getAll(LocalDate manufacturedDate) {
         List<RollManufactured> rollManufacturedList = rollManufacturedService.findAll(manufacturedDate);
         return getAll(rollManufacturedList);
     }
 
+    /**
+     * Вычисляет и возвращает информацию о количестве произведнных, списанных в производство и оставшихся
+     * на данный момент рулонов, произведенных в указанный период времени
+     *
+     * @param fromManufacturedDate начало периода даты производства рулонов
+     * @param toManufacturedDate окончание периода даты производства рулонов
+     * @return {@code List} {@code RollBatch} для рулонов, произведенных в указанный период времени
+     * или empty {@code List}, если в базе отсутствуют рулоны, произведенные в указанный период
+     * @throws IllegalArgumentException если fromManufacturedDate - null и/или toManufacturedDate - null
+     */
     @Override
     public List<RollBatch> getAll(LocalDate fromManufacturedDate, LocalDate toManufacturedDate) {
         List<RollManufactured> rollManufacturedList =
@@ -37,6 +76,17 @@ public class RollBatchServiceImpl implements RollBatchService {
         return getAll(rollManufacturedList);
     }
 
+    /**
+     * Вычисляет и возвращает информацию о количестве произведнных, списанных в производство и оставшихся
+     * на данный момент рулонов указанного типа, произведенных в указанный период времени
+     *
+     * @param rollTypeId ID типа рулона
+     * @param fromManufacturedDate начало периода даты производства рулонов
+     * @param toManufacturedDate окончание периода даты производства рулонов
+     * @return {@code List} {@code RollBatch} для рулонов указанного типа, произведенных в указанный период времени
+     * или empty {@code List}, если в базе отсутствуют рулоны с указанным типом, произведенные в указанный период
+     * @throws IllegalArgumentException если fromManufacturedDate - null и/или toManufacturedDate - null
+     */
     @Override
     public List<RollBatch> getAll(Long rollTypeId, LocalDate fromManufacturedDate, LocalDate toManufacturedDate) {
         List<RollManufactured> rollManufacturedList =
@@ -44,6 +94,13 @@ public class RollBatchServiceImpl implements RollBatchService {
         return getAll(rollManufacturedList);
     }
 
+    /**
+     * Вычисляет и возвращает информацию о количестве произведнных, списанных в производство и оставшихся
+     * на данный момент рулонов для указанного рулона {@code RollManufactured}
+     * // todo description for RollManufactured
+     * @param rollManufactured рулон {@code RollManufactured}
+     * @return {@code RollBatch} для указанного рулона
+     */
     private RollBatch getOne(RollManufactured rollManufactured) {
         Integer manufacturedAmount = rollManufacturedService.getManufacturedRollAmount(rollManufactured);
         Integer usedAmount = rollManufacturedService.getUsedRollAmount(rollManufactured);
@@ -54,6 +111,13 @@ public class RollBatchServiceImpl implements RollBatchService {
         return rollBatch;
     }
 
+    /**
+     * Вычисляет и возвращает информацию о количестве произведнных, списанных в производство и оставшихся
+     * на данный момент рулонов для указанного списка рулонов {@code RollManufactured}
+     * // todo description for RollManufactured
+     * @param rollManufacturedList {@code List} рулонов {@code RollManufactured}
+     * @return {@code List} {@code RollBatch} для указанного списка рулона
+     */
     private List<RollBatch> getAll(List<RollManufactured> rollManufacturedList) {
         return rollManufacturedList.stream()
                 .map(this::getOne)
