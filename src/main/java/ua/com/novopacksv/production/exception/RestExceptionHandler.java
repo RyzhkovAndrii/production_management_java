704e605bb97176cbe25b8c4992bb9826e9c73ad0
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-04-20 11:50:39 +0300

    added not valid exception handler

--- src/main/java/ua/com/novopacksv/production/exception/RestExceptionHandler.java
+++ src/main/java/ua/com/novopacksv/production/exception/RestExceptionHandler.java
@@ -3,16 +3,16 @@ package ua.com.novopacksv.production.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Optional;
 
 @RestControllerAdvice
 @RequestMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+public class RestExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ExceptionResponse> handleEntityNotFound(ResourceNotFoundException ex) {
@@ -23,10 +23,20 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(NegativeRollAmountException.class)
-    public ResponseEntity<ExceptionResponse> handleNegativeRollAmount(NegativeRollAmountException ex){
+    public ResponseEntity<ExceptionResponse> handleNegativeRollAmount(NegativeRollAmountException ex) {
         String message = Optional.of(ex.getMessage()).orElse(ex.getClass().getSimpleName());
         ExceptionResponse response = new ExceptionResponse();
         response.setMessage(message);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<NotValidExceptionResponse> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        NotValidExceptionResponse response = new NotValidExceptionResponse();
+        response.setStatus(400);
+        response.setError("Bad Request");
+        ex.getBindingResult().getFieldErrors().forEach((fieldError) ->
+                response.addFieldException(fieldError.getField(), fieldError.getDefaultMessage()));
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
