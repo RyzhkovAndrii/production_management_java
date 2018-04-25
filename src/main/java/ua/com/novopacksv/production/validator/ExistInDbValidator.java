01aa71f125a1609b814819fb04058a9e8d16e624
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-04-25 15:57:51 +0300

    added custom validator for check is entity in db by id

---
+++ src/main/java/ua/com/novopacksv/production/validator/ExistInDbValidator.java
@@ -0,0 +1,28 @@
+package ua.com.novopacksv.production.validator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+@Component
+public class ExistInDbValidator implements ConstraintValidator<ExistInDb, Long> {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    private Class<?> clazz;
+
+    @Override
+    public void initialize(ExistInDb existInDb) {
+        this.clazz = existInDb.value();
+    }
+
+    @Override
+    public boolean isValid(Long id, ConstraintValidatorContext constraintValidatorContext) {
+        return entityManager.find(clazz, id) != null;
+    }
+
+}
