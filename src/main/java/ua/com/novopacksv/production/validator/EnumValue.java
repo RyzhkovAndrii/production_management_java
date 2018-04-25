530999f20707b7de47561860f96269a891942a8b
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-04-25 15:57:04 +0300

    added custom enum value validator

---
+++ src/main/java/ua/com/novopacksv/production/validator/EnumValue.java
@@ -0,0 +1,23 @@
+package ua.com.novopacksv.production.validator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = EnumValueValidator.class)
+public @interface EnumValue {
+
+    Class<? extends Enum<?>> value();
+
+    String message() default "value is not in Enum!";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
---
+++ src/main/java/ua/com/novopacksv/production/validator/EnumValueValidator.java
@@ -0,0 +1,24 @@
+package ua.com.novopacksv.production.validator;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.Arrays;
+
+public class EnumValueValidator implements ConstraintValidator<EnumValue, String> {
+
+    private Class<? extends Enum<?>> enumClass;
+
+    public void initialize(EnumValue enumValue) {
+        enumClass = enumValue.value();
+    }
+
+    public boolean isValid(String stringValue, ConstraintValidatorContext context) {
+        return isValueInEnum(stringValue, enumClass);
+    }
+
+    private boolean isValueInEnum(String stringValue, Class<? extends Enum<?>> enumClass) {
+        return Arrays.stream(enumClass.getEnumConstants())
+                .anyMatch((enumValue) -> enumValue.name().equals(stringValue.toUpperCase()));
+    }
+
+}
