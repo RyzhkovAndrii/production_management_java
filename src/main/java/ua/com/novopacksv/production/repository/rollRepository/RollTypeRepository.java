94bf9b066b96e5a886e8a95e88367666f86ca4a4
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-04-25 16:58:12 +0300

    added findOne(String name) method

--- src/main/java/ua/com/novopacksv/production/repository/rollRepository/RollTypeRepository.java
+++ src/main/java/ua/com/novopacksv/production/repository/rollRepository/RollTypeRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ua.com.novopacksv.production.model.rollModel.RollType;
 
+import java.util.Optional;
+
 @Repository
 public interface RollTypeRepository extends JpaRepository <RollType, Long> {
+
+    Optional<RollType> findOne(String name);
+
 }
