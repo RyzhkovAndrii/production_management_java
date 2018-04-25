6c489279aeaec19ff5e5323c0d9cba76e4612b22
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-04-25 12:44:42 +0300

    changed validation messages

--- src/main/java/ua/com/novopacksv/production/dto/roll/RollTypeRequest.java
+++ src/main/java/ua/com/novopacksv/production/dto/roll/RollTypeRequest.java
@@ -11,21 +11,21 @@ import javax.validation.constraints.*;
 @Setter
 public class RollTypeRequest {
 
-    @NotBlank(message = "roll type's name is a required field!")
-    @Size(max = 50, message = "roll type's name must be less then 50 symbols long!")
+    @NotBlank(message = "roll type name is a required field!")
+    @Size(max = 50, message = "roll type name must be less then 50 symbols long!")
     @Unique(value = RollType.class, column = "name", message = "roll type's name must be unique!")
     private String name;
 
-    @NotNull(message = "roll's thickness is a required field!")
-    @Positive(message = "roll's thickness must be greater then 0!")
+    @NotNull(message = "roll thickness is a required field!")
+    @Positive(message = "roll thickness must be greater then 0!")
     private Double thickness;
 
-    @NotNull(message = "roll's weight is a required field!")
-    @Positive(message = "roll's weight must be greater then 0!")
+    @NotNull(message = "roll weight is a required field!")
+    @Positive(message = "roll weight must be greater then 0!")
     private Double weight;
 
-    @NotNull(message = "roll's color code is a required field!")
-    @Pattern(regexp = "#([0-9A-Fa-f]{6})", message = "incorrect roll's color code format!")
+    @NotNull(message = "roll color code is a required field!")
+    @Pattern(regexp = "#([0-9A-Fa-f]{6})", message = "incorrect roll color code format!")
     private String colorCode;
 
 }
