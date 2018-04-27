da2fa934a69377b60d07fd874eb307ad62d1e428
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-04-27 17:49:24 +0300

    removed methods

--- src/main/java/ua/com/novopacksv/production/service/order/OrderService.java
+++ src/main/java/ua/com/novopacksv/production/service/order/OrderService.java
@@ -4,9 +4,4 @@ import ua.com.novopacksv.production.model.orderModel.Order;
 import ua.com.novopacksv.production.service.BaseEntityService;
 
 public interface OrderService extends BaseEntityService<Order> {
-
-    void addOrderItemToOrder(Long orderId, Long OrderItemId);
-
-    void removeOrderItemFromOrder(Long orderId, Long OrderItemId);
-
 }
--- src/main/java/ua/com/novopacksv/production/service/order/OrderServiceImpl.java
+++ src/main/java/ua/com/novopacksv/production/service/order/OrderServiceImpl.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ua.com.novopacksv.production.exception.ResourceNotFoundException;
 import ua.com.novopacksv.production.model.orderModel.Order;
-import ua.com.novopacksv.production.model.orderModel.OrderItem;
 import ua.com.novopacksv.production.repository.orderRepository.OrderRepository;
 
 import java.util.List;
@@ -16,8 +15,6 @@ import java.util.List;
 public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
-
-    private final OrderItemService orderItemService;
 
     @Override
     @Transactional(readOnly = true)
@@ -47,22 +44,6 @@ public class OrderServiceImpl implements OrderService {
     @Override
     public void delete(Long id) {
         orderRepository.delete(findById(id));
-    }
-
-    @Override
-    public void addOrderItemToOrder(Long orderId, Long orderItemId) {
-        Order order = findById(orderId);
-        OrderItem orderItem = orderItemService.findById(orderItemId);
-        order.getOrderItems().add(orderItem);
-        orderItem.setOrder(order);
-    }
-
-    @Override
-    public void removeOrderItemFromOrder(Long orderId, Long orderItemId) {
-        Order order = findById(orderId);
-        OrderItem orderItem = orderItemService.findById(orderItemId);
-        order.getOrderItems().remove(orderItem);
-        orderItem.setOrder(null);
     }
 
 }
