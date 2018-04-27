4a5a43028a0a8d4301c150f02c581fe68b918db4
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-04-27 17:23:41 +0300

    added method finding client by name

--- src/main/java/ua/com/novopacksv/production/service/order/ClientService.java
+++ src/main/java/ua/com/novopacksv/production/service/order/ClientService.java
@@ -4,4 +4,7 @@ import ua.com.novopacksv.production.model.orderModel.Client;
 import ua.com.novopacksv.production.service.BaseEntityService;
 
 public interface ClientService extends BaseEntityService<Client> {
+
+    Client findOne(String name);
+
 }
