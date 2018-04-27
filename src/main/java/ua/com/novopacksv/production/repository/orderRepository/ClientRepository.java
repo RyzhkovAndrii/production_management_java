2b939dc600efd38d97d2ba9057d16e89e86c7e80
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-04-27 17:32:57 +0300

    added findByName method

--- src/main/java/ua/com/novopacksv/production/repository/orderRepository/ClientRepository.java
+++ src/main/java/ua/com/novopacksv/production/repository/orderRepository/ClientRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ua.com.novopacksv.production.model.orderModel.Client;
 
+import java.util.Optional;
+
 @Repository
 public interface ClientRepository extends JpaRepository <Client, Long> {
+
+    Optional<Client> findByName(String name);
+
 }
