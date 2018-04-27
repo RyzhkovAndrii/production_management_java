55c1d05cf1fb24218d135ba610b71c9825306220
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-04-27 17:46:33 +0300

    implemented methods

--- src/main/java/ua/com/novopacksv/production/service/order/OrderServiceImpl.java
+++ src/main/java/ua/com/novopacksv/production/service/order/OrderServiceImpl.java
@@ -1,48 +1,68 @@
 package ua.com.novopacksv.production.service.order;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ua.com.novopacksv.production.exception.ResourceNotFoundException;
 import ua.com.novopacksv.production.model.orderModel.Order;
+import ua.com.novopacksv.production.model.orderModel.OrderItem;
+import ua.com.novopacksv.production.repository.orderRepository.OrderRepository;
 
 import java.util.List;
 
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
-    @Override
-    public void addOrderItemToOrder(Long orderId, Long OrderItemId) {
+    private final OrderRepository orderRepository;
 
-    }
-
-    @Override
-    public void removeOrderItemFromOrder(Long orderId, Long OrderItemId) {
-
-    }
+    private final OrderItemService orderItemService;
 
     @Override
+    @Transactional(readOnly = true)
     public Order findById(Long id) {
-        return null;
+        return orderRepository.findById(id).orElseThrow(() -> {
+            String message = String.format("Order whit id = %d is not found!", id);
+            return new ResourceNotFoundException(message);
+        });
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Order> findAll() {
-        return null;
+        return orderRepository.findAll();
     }
 
     @Override
     public Order save(Order order) {
-        return null;
+        return orderRepository.save(order);
     }
 
     @Override
     public Order update(Order order) {
-        return null;
+        return save(order);
     }
 
     @Override
     public void delete(Long id) {
+        orderRepository.delete(findById(id));
+    }
 
+    @Override
+    public void addOrderItemToOrder(Long orderId, Long orderItemId) {
+        Order order = findById(orderId);
+        OrderItem orderItem = orderItemService.findById(orderItemId);
+        order.getOrderItems().add(orderItem);
+        orderItem.setOrder(order);
+    }
+
+    @Override
+    public void removeOrderItemFromOrder(Long orderId, Long orderItemId) {
+        Order order = findById(orderId);
+        OrderItem orderItem = orderItemService.findById(orderItemId);
+        order.getOrderItems().remove(orderItem);
+        orderItem.setOrder(null);
     }
 
 }
