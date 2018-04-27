9e3faf991f398e810fbcb1a79d9270adf8fb4e0b
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-04-27 17:38:13 +0300

    added findByOrder method

--- src/main/java/ua/com/novopacksv/production/repository/orderRepository/OrderItemRepository.java
+++ src/main/java/ua/com/novopacksv/production/repository/orderRepository/OrderItemRepository.java
@@ -2,8 +2,14 @@ package ua.com.novopacksv.production.repository.orderRepository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ua.com.novopacksv.production.model.orderModel.Order;
 import ua.com.novopacksv.production.model.orderModel.OrderItem;
+
+import java.util.List;
 
 @Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+    List<OrderItem> findByOrder(Order order);
+
 }
