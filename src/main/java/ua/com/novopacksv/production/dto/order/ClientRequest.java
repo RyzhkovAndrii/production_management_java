7c2d6835eba8c4f7326c2fb0900fa974064d5a3b
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-04-27 16:57:05 +0300

    added validation for fields

--- src/main/java/ua/com/novopacksv/production/dto/order/ClientRequest.java
+++ src/main/java/ua/com/novopacksv/production/dto/order/ClientRequest.java
@@ -3,10 +3,15 @@ package ua.com.novopacksv.production.dto.order;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 public class ClientRequest {
 
+    @NotBlank(message = "client name is a required field!")
+    @Size(max = 50, message = "client name must be less then 50 symbols long!")
     private String name;
 
 }
