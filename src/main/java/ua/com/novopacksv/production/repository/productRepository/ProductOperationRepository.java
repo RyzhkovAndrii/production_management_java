f3bba3b4f6a30e1311dbae457fec9612a8a38c0f
Olesiap <pryadilnikova@gmail.com>
2018-04-28 23:32:59 +0300

    added method findByDateBetween in ProductOperationRepository

--- src/main/java/ua/com/novopacksv/production/repository/productRepository/ProductOperationRepository.java
+++ src/main/java/ua/com/novopacksv/production/repository/productRepository/ProductOperationRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ua.com.novopacksv.production.model.productModel.ProductOperation;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface ProductOperationRepository extends JpaRepository<ProductOperation, Long> {
+
+    List<ProductOperation> findAllByDateBetween(LocalDate fromDate, LocalDate toDate);
 }
