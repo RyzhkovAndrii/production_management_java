bad05fde3df7a5ff1afd6f4edda4376f3320c801
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-07-05 23:16:51 +0300

    removed @FutureOrPresent validation from "deliveryDate" field

--- src/main/java/ua/com/novopacksv/production/dto/order/OrderRequest.java
+++ src/main/java/ua/com/novopacksv/production/dto/order/OrderRequest.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.Setter;
 import ua.com.novopacksv.production.model.orderModel.Client;
 import ua.com.novopacksv.production.validator.ExistInDb;
-import ua.com.novopacksv.production.validator.FutureOrPresent;
 import ua.com.novopacksv.production.validator.LocalDateFormat;
 import ua.com.novopacksv.production.validator.PastOrPresent;
 
@@ -26,13 +25,12 @@ public class OrderRequest {
 
     @NotBlank(message = "delivery date is a required field!")
     @LocalDateFormat(message = "incorrect delivery date format!")
-    @FutureOrPresent(message = "delivery date can not be in past!")
     private String deliveryDate;
 
     @NotNull(message = "is_important is a required field!")
     private Boolean isImportant;
 
-    @LocalDateFormat(message = "incorrect delivery date format!")
+    @LocalDateFormat(message = "incorrect actual delivery date format!")
     @PastOrPresent(message = "actual delivery date can not be in future!")
     private String actualDeliveryDate;
 
