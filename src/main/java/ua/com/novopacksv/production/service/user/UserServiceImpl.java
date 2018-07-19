825b4a95913d0f1a8216d4e203706db0dd5bcc60
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-07-19 15:08:38 +0300

    added find by username method

--- src/main/java/ua/com/novopacksv/production/service/user/UserServiceImpl.java
+++ src/main/java/ua/com/novopacksv/production/service/user/UserServiceImpl.java
@@ -1,37 +1,52 @@
 package ua.com.novopacksv.production.service.user;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ua.com.novopacksv.production.exception.ResourceNotFoundException;
+import ua.com.novopacksv.production.model.userModel.User;
+import ua.com.novopacksv.production.repository.userRepository.UserRepository;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @Service
 @Transactional
 public class UserServiceImpl implements UserService {
 
+    private final UserRepository userRepository;
+
     @Override
-    public ua.com.novopacksv.production.model.userModel.User findById(Long id) {
+    public User findById(Long id) {
         return null;
     }
 
     @Override
-    public List<ua.com.novopacksv.production.model.userModel.User> findAll() {
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User save(User user) {
         return null;
     }
 
     @Override
-    public ua.com.novopacksv.production.model.userModel.User save(ua.com.novopacksv.production.model.userModel.User user) {
-        return null;
-    }
-
-    @Override
-    public ua.com.novopacksv.production.model.userModel.User update(ua.com.novopacksv.production.model.userModel.User user) {
+    public User update(User user) {
         return null;
     }
 
     @Override
     public void delete(Long id) {
 
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return this.userRepository.findByUsername(username).orElseThrow(() -> {
+            String message = String.format("User whit username = %s is not found!", username);
+            return new ResourceNotFoundException(message);
+        });
     }
 
 }
