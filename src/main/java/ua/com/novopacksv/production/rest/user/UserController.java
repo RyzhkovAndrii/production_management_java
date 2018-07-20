85aad855596ff92ab109d1d25f9800a307fa8353
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-07-20 14:49:18 +0300

    filled user service and controller

--- src/main/java/ua/com/novopacksv/production/rest/user/UserController.java
+++ src/main/java/ua/com/novopacksv/production/rest/user/UserController.java
@@ -1,19 +1,65 @@
 package ua.com.novopacksv.production.rest.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import ua.com.novopacksv.production.service.user.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import ua.com.novopacksv.production.converter.ModelConversionService;
+import ua.com.novopacksv.production.dto.user.UserRequest;
+import ua.com.novopacksv.production.dto.user.UserResponse;
+import ua.com.novopacksv.production.model.userModel.User;
+import ua.com.novopacksv.production.service.user.UserService;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
     private final ModelConversionService conversionService;
+
+    @GetMapping
+    public ResponseEntity<List<UserResponse>> getAll() {
+        List<User> users = userService.findAll();
+        List<UserResponse> response = conversionService.convert(users, UserResponse.class);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> getOne(@PathVariable Long id) {
+        User user = userService.findById(id);
+        UserResponse response = conversionService.convert(user, UserResponse.class);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<UserResponse> save(@Valid @RequestBody UserRequest request) {
+        User user = conversionService.convert(request, User.class);
+        user = userService.save(user);
+        UserResponse response = conversionService.convert(user, UserResponse.class);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponse> update(@PathVariable Long id, @Valid @RequestBody UserRequest request) {
+        User user = conversionService.convert(request, User.class);
+        user.setId(id);
+        user = userService.update(user);
+        UserResponse response = conversionService.convert(user, UserResponse.class);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        userService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
 }
--- src/main/java/ua/com/novopacksv/production/service/user/UserServiceImpl.java
+++ src/main/java/ua/com/novopacksv/production/service/user/UserServiceImpl.java
@@ -3,11 +3,13 @@ package ua.com.novopacksv.production.service.user;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ua.com.novopacksv.production.exception.NotUniqueFieldException;
 import ua.com.novopacksv.production.exception.ResourceNotFoundException;
 import ua.com.novopacksv.production.model.userModel.User;
 import ua.com.novopacksv.production.repository.userRepository.UserRepository;
 
 import java.util.List;
+import java.util.Objects;
 
 @RequiredArgsConstructor
 @Service
@@ -17,36 +19,57 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
 
     @Override
+    @Transactional(readOnly = true)
     public User findById(Long id) {
-        return null;
+        return userRepository.findById(id).orElseThrow(() -> {
+            String message = String.format("User whit id = %d is not found!", id);
+            return new ResourceNotFoundException(message);
+        });
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<User> findAll() {
         return userRepository.findAll();
     }
 
     @Override
     public User save(User user) {
-        return null;
+        checkUsernameUnique(user);
+        return userRepository.save(user);
     }
 
     @Override
     public User update(User user) {
-        return null;
+        findById(user.getId());
+        return save(user);
     }
 
     @Override
     public void delete(Long id) {
-
+        userRepository.deleteById(id);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public User findByUsername(String username) {
         return this.userRepository.findByUsername(username).orElseThrow(() -> {
             String message = String.format("User whit username = %s is not found!", username);
             return new ResourceNotFoundException(message);
         });
+    }
+
+    private void checkUsernameUnique(User user) {
+        User entityUser = userRepository.findByUsername(user.getUsername()).orElse(null);
+        if (entityUser != null && !hasSameId(user, entityUser)) {
+            throw new NotUniqueFieldException("Username must be unique!");
+        }
+    }
+
+    private boolean hasSameId(User user, User entityUser) {
+        Long id = user.getId();
+        Long entityId = entityUser.getId();
+        return id != null && entityId != null && Objects.equals(id, entityId);
     }
 
 }
