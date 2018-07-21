ac3aca28da84c7b8513a6ddf6e2973284af111b3
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-07-21 13:51:07 +0300

    added fields validation

--- src/main/java/ua/com/novopacksv/production/dto/user/UserRequest.java
+++ src/main/java/ua/com/novopacksv/production/dto/user/UserRequest.java
@@ -2,21 +2,35 @@ package ua.com.novopacksv.production.dto.user;
 
 import lombok.Getter;
 import lombok.Setter;
+import ua.com.novopacksv.production.model.userModel.Role;
+import ua.com.novopacksv.production.validator.EnumValue;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Getter
 @Setter
 public class UserRequest {
 
+    @NotBlank(message = "username is a required field!")
+    @Size(max = 30, message = "username must be less then 30 symbols long!")
     private String username;
 
+    @NotBlank(message = "password is a required field!")
+    @Size(max = 30, message = "password must be less then 30 symbols long!")
     private String password;
 
+    @NotBlank(message = "first name is a required field!")
+    @Size(max = 30, message = "first name must be less then 30 symbols long!")
     private String firstName;
 
+    @NotBlank(message = "last name is a required field!")
+    @Size(max = 30, message = "last name must be less then 30 symbols long!")
     private String lastName;
 
-    private List<String> roles;
+    @NotEmpty(message = "roles is a required field!")
+    private List<@EnumValue(value = Role.class, message = "role is not found!") String> roles;
 
 }
--- src/main/java/ua/com/novopacksv/production/validator/EnumValue.java
+++ src/main/java/ua/com/novopacksv/production/validator/EnumValue.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.METHOD, ElementType.FIELD})
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.TYPE_USE})
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = EnumValueValidator.class)
 public @interface EnumValue {
