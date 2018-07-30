ea97a7d82c503710606915938904b99e6fe964c0
Olesiap <pryadilnikova@gmail.com>
2018-07-30 22:57:48 +0300

    added CRUD to MachinePlanService

--- src/main/java/ua/com/novopacksv/production/service/plan/MachinePlanServiceImpl.java
+++ src/main/java/ua/com/novopacksv/production/service/plan/MachinePlanServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ua.com.novopacksv.production.exception.ResourceNotFoundException;
 import ua.com.novopacksv.production.model.planModel.MachinePlan;
+import ua.com.novopacksv.production.repository.planRepository.MachinePlanRepository;
 
 import java.util.List;
 
@@ -15,28 +16,43 @@ import java.util.List;
 @Slf4j
 public class MachinePlanServiceImpl implements MachinePlanService {
 
+    private final MachinePlanRepository machinePlanRepository;
+
     @Override
     public MachinePlan findById(Long id) throws ResourceNotFoundException {
-        return null;
+        MachinePlan machinePlan = machinePlanRepository.findById(id).orElseThrow(() -> {
+            String message = String.format("MachinePlan with id = %d was not found", id);
+            log.error("Method findById(Long id): MachinePlan with id = {} was not found", id);
+            return new ResourceNotFoundException(message);
+        });
+        log.debug("Method findById(Long id): MachinePlan with id = {} was found: {}", id, machinePlan);
+        return machinePlan;
     }
 
     @Override
     public List<MachinePlan> findAll() {
-        return null;
+        log.debug("Method findAll(): List<MachinePlan> is finding");
+        return machinePlanRepository.findAll();
     }
 
     @Override
     public MachinePlan save(MachinePlan machinePlan) {
-        return null;
+        log.debug("Method save(MachinePlan machinePlan): MachinePlan {} was saved", machinePlan);
+        return machinePlanRepository.save(machinePlan);
     }
 
     @Override
     public MachinePlan update(MachinePlan machinePlan) throws ResourceNotFoundException {
-        return null;
+        findById(machinePlan.getId());
+        machinePlanRepository.save(machinePlan);
+        MachinePlan machinePlanUpdated = findById(machinePlan.getId());
+        log.debug("Method update(MachinePlan machinePlan): MachinePlan {} was updated", machinePlanUpdated);
+        return machinePlanUpdated;
     }
 
     @Override
     public void delete(Long id) throws ResourceNotFoundException {
-
+        machinePlanRepository.delete(findById(id));
+        log.debug("Method delete(Long id): MachinePlane with id = {} was deleted", id);
     }
 }
