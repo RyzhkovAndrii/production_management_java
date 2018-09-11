21eb5d8a3069eda2a6ddb59c81c14333619045cf
Olesiap <pryadilnikova@gmail.com>
2018-09-11 23:57:52 +0300

    xx

--- src/test/java/ua/com/novopacksv/production/service/roll/RollTypeServiceImplTest.java
+++ src/test/java/ua/com/novopacksv/production/service/roll/RollTypeServiceImplTest.java
@@ -1,17 +1,18 @@
 package ua.com.novopacksv.production.service.roll;
 
-import org.junit.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 import ua.com.novopacksv.production.exception.ResourceNotFoundException;
 import ua.com.novopacksv.production.model.rollModel.RollType;
-import org.springframework.security.test.context.support.WithMockUser;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -82,7 +83,7 @@ public class RollTypeServiceImplTest {
 
     @Test
     @WithMockUser("cmo")
-    public void save() {   //TODO add user
+    public void save() {
         //init for 2-nd model
         rollType1.setColorCode("#ffffff");
         rollType1.setLength(380.0);
