d23a4715205699963ab4c674c3657b5774e1e36d
Olesiap <pryadilnikova@gmail.com>
2018-09-18 02:16:04 +0300

    added TestSuite

---
+++ src/test/java/ua/com/novopacksv/production/TestSuite.java
@@ -0,0 +1,13 @@
+package ua.com.novopacksv.production;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import ua.com.novopacksv.production.service.roll.RollManufacturedServiceImplTest;
+import ua.com.novopacksv.production.service.roll.RollOperationServiceImplTest;
+import ua.com.novopacksv.production.service.roll.RollTypeServiceImplTest;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({RollTypeServiceImplTest.class, RollOperationServiceImplTest.class,
+        RollManufacturedServiceImplTest.class})
+public class TestSuite {
+}
