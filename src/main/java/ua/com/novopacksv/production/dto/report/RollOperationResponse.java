182c098568ea75f9a552916b7a62f81558e5c3f7
andrii <ryzhkov.andrii@gmail.com>
2019-01-14 22:54:54 +0200

    added roll operation response for norm report

---
+++ src/main/java/ua/com/novopacksv/production/dto/report/RollOperationResponse.java
@@ -0,0 +1,14 @@
+package ua.com.novopacksv.production.dto.report;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RollOperationResponse {
+
+    private long rollTypeId;
+
+    private int amount;
+
+}
